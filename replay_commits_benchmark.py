4adec9054d30db289e7e02a2e1f062552e87037a
webster <michael@webster.fyi>
2024-08-20 20:22:43 -0400

    Somehow I hadn't added the benchmakr ignore. Maybe it was when I moved it to configs?

--- replay_commits_benchmark.py
+++ replay_commits_benchmark.py
@@ -327,9 +327,9 @@ def push_commits_one_by_one(args, repo, commits):
                   folders_from_main[item.path] = BytesIO(item.data_stream.read()).getvalue()
 
       repo.head.reset(commit=commit, index=True, working_tree=True)
-      #shutil.move(os.path.join(args['working_repo_dir'], config_path, '.gitignore_benchmark'), os.path.join(args['working_repo_dir'], '.gitignore'))
-      #repo.git.add('.')
-      #repo.index.commit("Committing gitignore")
+      shutil.move(os.path.join(args['working_repo_dir'], config_path, '.gitignore_benchmark'), os.path.join(args['working_repo_dir'], '.gitignore'))
+      repo.git.add('.')
+      repo.index.commit("Committing gitignore")
       for path, data in folders_from_main.items():
           if os.path.exists(path):
               os.remove(path)
